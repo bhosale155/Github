72ebaaa6732363c7465c5bc01d843346de5934e6
PC <PC@Pooja-1Bhosale>
2022-08-08 09:44:14 +0530

    [Bhosale]Hi Team Move to New Project

---
+++ src/August/demo12.java
@@ -0,0 +1,13 @@
+package August;
+
+public class demo12 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+		System.out.println("GM");
+		System.out.println("Welcome Team");
+		System.out.println("Git");
+	}
+
+}
